95f0bbde46d4aea6c01d89b80bf9984cd9198519
Stevie Holdway <stevie.holdway@gmail.com>
2021-02-07 13:54:49 +0000

    More hash shortcuts

--- emacs.d/macos.el
+++ emacs.d/macos.el
@@ -1,5 +1,9 @@
 (when (string= system-type "darwin")
-  (global-set-key (kbd "M-3") (lambda () (interactive) (insert "#")))
+  (defun hash ()
+    (interactive)
+    (insert "#"))
+  (global-set-key (kbd "M-3") 'hash)
+  (global-set-key (kbd "C-c 3") 'hash)
   (use-package exec-path-from-shell
     :ensure t
     :config (exec-path-from-shell-initialize))
