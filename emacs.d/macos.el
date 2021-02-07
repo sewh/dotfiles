a09340b4f79cc2c1dd00b27c8c98b4d1b0746de1
Stevie Holdway <stevie.holdway@gmail.com>
2021-02-07 10:44:42 +0000

    Added new hash key binding

--- emacs.d/macos.el
+++ emacs.d/macos.el
@@ -1,5 +1,6 @@
 (when (string= system-type "darwin")
   (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+  (global-set-key (kbd "C-q") '(lambda () (interactive) (insert "#")))
   (use-package exec-path-from-shell
     :ensure t
     :config (exec-path-from-shell-initialize))
