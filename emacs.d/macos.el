5b2d113ff997f4a4ddc09f121f2d446c1b10eaf6
Stevie Holdway <stevie.holdway@gmail.com>
2021-02-07 11:06:45 +0000

    Fixed macos keybindings

--- emacs.d/macos.el
+++ emacs.d/macos.el
@@ -1,6 +1,5 @@
 (when (string= system-type "darwin")
-  (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-  (global-set-key (kbd "C-q") '(lambda () (interactive) (insert "#")))
+  (global-set-key (kbd "M-3") (lambda () (interactive) (insert "#")))
   (use-package exec-path-from-shell
     :ensure t
     :config (exec-path-from-shell-initialize))
