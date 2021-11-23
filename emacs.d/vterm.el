e54ab81a3f2ca8fcaa860fec56e09d68dc0b4076
Stevie Holdway <stevie.holdway@gmail.com>
2021-11-23 21:51:24 +0000

    added vterm-named

--- emacs.d/vterm.el
+++ emacs.d/vterm.el
@@ -1,2 +1,9 @@
 (use-package vterm
   :ensure t)
+
+(defun vterm-named (name)
+  (interactive "sTerminal name: ")
+  (let ((term-name (concat "vterm-" name)))
+    (vterm term-name)))
+
+(global-set-key (kbd "C-x v") #'vterm-named)
