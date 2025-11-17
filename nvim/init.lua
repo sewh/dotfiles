c46edc30817d3e1f262684963a41a44b0bacf01d
Stevie Holdway <stevie.holdway@gmail.com>
2025-11-17 11:51:49 +0000

    added treesitter indent

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,6 +112,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
 
     disable = { },
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
+  indent = { enable = true },
 }
