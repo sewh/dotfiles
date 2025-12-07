ab77cb02299dd215cf441205b34b44f9550542a7
Stevie Holdway <stevie.holdway@gmail.com>
2025-12-07 16:07:04 +0000

    removed plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,3 @@
--- plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- leader!
 vim.g.mapleader = " "
 
@@ -102,30 +85,3 @@ vim.lsp.config["rust"] = {
 }
 vim.lsp.enable({ "rust" })
 
--- plugins
-require("lazy").setup({
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-})
-
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { 
-    -- "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
-    "elixir"
-  },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-
-    disable = { },
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true },
-}
