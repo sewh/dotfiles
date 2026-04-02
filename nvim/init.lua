3e4d595175b3b35a9651e9b0873a8ec59791c0e6
Stevie Holdway <stevie.holdway@gmail.com>
2026-04-02 12:43:00 +0100

    Got Claude to Clean up Neovim Config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- leader!
+-- leader
 vim.g.mapleader = " "
 
 -- plugins
@@ -10,7 +10,9 @@ vim.pack.add({
   { src = "https://github.com/folke/which-key.nvim.git" },
 })
 
--- basic sets
+-- options
+
+-- indentation
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -20,27 +22,34 @@ vim.opt.smartindent = true
 vim.opt.smarttab = true
 vim.opt.list = true
 
+-- ui
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.signcolumn = "yes:1"
 vim.opt.scrolloff = 8
 vim.opt.showcmd = true
 
+-- persistence
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
-vim.opt.clipboard = "unnamed"
 
+-- search
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- disable auto-comment continuation
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+
+-- appearance
 vim.opt.termguicolors = true
 require('ayu').setup({})
 vim.cmd([[colorscheme ayu-dark]])
 vim.opt.showmode = false
+
 require('which-key').setup({})
 require('lualine').setup({
   options = {
@@ -51,10 +60,7 @@ require('lualine').setup({
   },
 })
 
--- No automatic comment insertion
-vim.cmd([[autocmd FileType * set formatoptions-=ro]])
-
--- clipboard
+-- clipboard (OSC 52 for remote copy/paste)
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
@@ -68,37 +74,34 @@ vim.g.clipboard = {
 }
 vim.opt.clipboard = "unnamedplus"
 
--- tabs
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
-vim.keymap.set('n', '<leader>tl', ':tabnext<CR>', { desc = 'Next tab' })
-vim.keymap.set('n', '<leader>th', ':tabprev<CR>', { desc = 'Previous tab' })
-vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close tab' })
-
--- extra syntax highlighting
+-- filetypes
 vim.filetype.add({
   filename = {
     ['SConstruct'] = 'python',
     ['SConscript'] = 'python',
   },
   pattern = {
-    -- Catches 'src/SConscript', 'hardware/SConscript', etc.
     ['.*SConscript'] = 'python',
     ['.*SConstruct'] = 'python',
   },
 })
 
--- lsp
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.expandtab = false  -- go uses tabs
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.list = false
+  end,
+})
+
+-- lsp servers
 vim.lsp.config["rust"] = {
-  -- Command and arguments to start the server.
   cmd = { 'rust-analyzer' },
-  -- Filetypes to automatically attach to.
   filetypes = { 'rust' },
-  -- Sets the "workspace" to the directory where any of these files is found.
-  -- Files that share a root directory will reuse the LSP server connection.
-  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
   root_markers = { '.editorconfig', '.git' },
-  -- Specific settings to send to the server. The schema is server-defined.
-  -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
   settings = {}
 }
 vim.lsp.enable({ "rust" })
@@ -107,32 +110,31 @@ vim.lsp.config["go"] = {
   cmd = { "gopls" },
   filetypes = { "go" },
   root_markers = { '.editorconfig', '.git', 'go.mod' },
-  settings = { }
+  settings = {}
 }
 vim.lsp.enable({ "go" })
 
 -- lsp keybindings
 
--- Completion
 vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { desc = 'LSP completion' })
 
--- Core navigation
+-- navigation
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Go to implementation' })
 vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = 'Go to type definition' })
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Find references' })
 
--- Documentation & help
+-- documentation
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover documentation' })
 vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature help' })
 
--- Code actions & refactoring
+-- actions
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code actions' })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = 'Format buffer' })
 
--- Diagnostics
+-- diagnostics
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
@@ -146,14 +148,8 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
 
--- go specific formatting
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function()
-    vim.opt_local.expandtab = false  -- Use actual tabs, not spaces
-    vim.opt_local.tabstop = 4        -- Display tabs as 4 spaces wide
-    vim.opt_local.shiftwidth = 4     -- Indent by 4 spaces when using >> or 
-    vim.opt_local.softtabstop = 4    -- Backspace deletes 4 spaces worth
-    vim.opt_local.list = false       -- Hide invisible characters for Go files
-  end,
-})
+-- tabs
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
+vim.keymap.set('n', '<leader>tl', ':tabnext<CR>', { desc = 'Next tab' })
+vim.keymap.set('n', '<leader>th', ':tabprev<CR>', { desc = 'Previous tab' })
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close tab' })
