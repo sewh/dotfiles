0b3377f0c37b061ec5e556aa85934d5d3b5748eb
Stevie Holdway <stevie.holdway@gmail.com>
2026-04-02 12:45:34 +0100

    Added Telescope Symbol Shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,6 +147,8 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Document symbols' })
+vim.keymap.set('n', '<leader>fS', builtin.lsp_workspace_symbols, { desc = 'Workspace symbols' })
 
 -- tabs
 vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
